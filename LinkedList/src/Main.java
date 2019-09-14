6a360a7e23fe1f86b46694bb838c790417a6d0cf
褚鹏飞 <450793585@qq.com>
2019-09-14 20:39:42 +0800

    链表实现

--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -1,11 +1,30 @@
 /**
  * @author louis
- *
  */
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("链表学习");
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        for (int i = 0; i < 5; i++) {
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+
+
+        linkedList.add(2, 666);
+        System.out.println(linkedList);
+
+        linkedList.remove(2);
+        System.out.println(linkedList);
+
+        linkedList.removeFirst();
+        System.out.println(linkedList);
+
+        linkedList.removeLast();
+        System.out.println();
+
     }
 }
