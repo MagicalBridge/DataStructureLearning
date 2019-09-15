4f8cc5b018e8bd4d52815614d3699f63ff5c8667
褚鹏飞 <450793585@qq.com>
2019-09-15 17:03:21 +0800

    二分搜索树插入元素

--- BinarySearchTree/src/BST.java
+++ BinarySearchTree/src/BST.java
@@ -41,12 +41,45 @@ public class BST<E extends Comparable<E>> {
     public int size() {
         return size;
     }
-    
+
     /**
      * BST 的成员函数
      */
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    /**
+     * 向二分搜索树中添加元素
+     */
+    public void add(E e) {
+        // 如果我们的根节点本身就是空的 元素直接赋值在根节点上
+        if (root == null) {
+            root = new Node(e);
+            size++;
+        } else {
+            add(root, e);
+        }
+    }
+
+    /**
+     * 向以node为根二分搜索树中插入元素E 递归算法
+     * 返回插入新节点后二分搜索树的根
+     */
+    private Node add(Node node, E e) {
+        // 如果 node 是空的 创建一个节点
+        if (node == null) {
+            size++;
+            return new Node(e);
+        }
+
+        if (e.compareTo(node.e) < 0) {
+            node.left = add(node.left, e);
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = add(node.right, e);
+        }
+
+        return node;
     }
 
 }
